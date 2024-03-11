cbcda73216efbf9e6b4b0dc9f37a50e13bb4cbd8
LUCAS DUEZ VERZARO DOS SANTOS <21.00306-8@maua.br>
2024-03-11 09:19:07 -0300

    Funcao main esta perguntando qual a opcao selecionada

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Olá, boas vindas à nossa incrível calculadora")
+
+opcao = 's'
+
+while opcao != 'n':
+    print("Digite qual operação deseja realizar a seguir:")
+    opcao = input("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n n - para parar o programa\n")
+
+    match opcao:
+        case '1':
+            pass
+        case '2':
+            pass
+        case '3':
+            pass
+        case '4':
+            pass
