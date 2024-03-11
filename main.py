2e8bce5731ed8e45041bf7ca4bcc3691ebc182e7
LUCAS DUEZ VERZARO DOS SANTOS <21.00306-8@maua.br>
2024-03-11 09:26:07 -0300

    funcao main finalizada

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
+import soma
+import Subtracao
+import multiplicacao
+import Divisao
+
 print("Olá, boas vindas à nossa incrível calculadora")
 
 opcao = 's'
 
 while opcao != 'n':
     print("Digite qual operação deseja realizar a seguir:")
-    opcao = input("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n n - para parar o programa\n")
+    opcao = input("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\nn - para parar o programa\n")
 
+    if(opcao == 'n'):
+        break
+
+    resultado = None
+
+    inputA = float(input("Digite o primeiro numero da operacao: "))
+    inputB = float(input("Digite o segundo numero da operacao: "))
     match opcao:
+
         case '1':
-            pass
+            resultado = soma.soma(inputA, inputB)
         case '2':
-            pass
+            resultado = Subtracao.funcaoSubtracao(inputA, inputB)
         case '3':
-            pass
+            resultado = multiplicacao.multiplicacao(inputA, inputB)
         case '4':
-            pass
+            resultado = Divisao.funcaoDivisao(inputA, inputB)
+        case _:
+            print("Operacao inválida!")
+
+    if(resultado is not None):
+        print("-----------------------------")
+        print(f"O resultado é: {resultado}")
+        resultado = None
+    
